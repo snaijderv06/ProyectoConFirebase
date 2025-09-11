26208f9376f0c3117015ebd16d2a338ea7257e12
snaijder <snaijdervargas46@gmail.com>
2025-09-11 17:02:10 -0600

    primeracarga

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { db } from './src/database/firebaseconfig';
+import { collection, getDocs } from 'firebase/firestore';
 
 export default function App() {
+
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "productos"));
+        const data = querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        //Preparación para subcolecciones (se ejecutará solo si existen)
+        const productosConSubcoleccion = await Promise.all(data.map(async (item)=> {
+          const subcoleccionSnapshot = await getDocs(collection(db, "productos", item.id, "sabores")).catch(() => []);
+          const subcoleccionData = subcoleccionSnapshot.docs.map(subDoc => ({
+            id: subDoc.id,
+            ...subDoc.data(),
+            parentId: item.id,
+          }));
+          return subcoleccionData.length > 0 ? { ...item, sabores: subcoleccionData} : item;
+        }));
+
+        setProductos(productosConSubcoleccion);
+      } catch (error) {
+        console.error("Error al obtener documentos: ", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Lista de Productos</Text>
+      <FlatList
+        data={productos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={styles.item}>
+             {item.nombre} - ${item.precio}
+            </Text>
+            {item.sabores && item.sabores.length > 0 && (
+              <FlatList
+                data={item.sabores}
+                keyExtractor={(subItem) => subItem.id}
+                renderItem={({ item: subItem}) => (
+                  <Text style={[styles.item, {marginLeft: 20}]}>
+                    Sabor: {subItem.sabor}
+                  </Text>
+                )}
+              />
+            )}
+            </View>
+          )}
+        />
     </View>
   );
 }
@@ -13,8 +67,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
-});
+  titulo: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  item: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+})
+;
